5be3ed13f131dec1db98f402c54962cd94ed9859
yor_email <daniil.stebnitsky@gmail.com>
2020-01-19 15:05:23 +0300

    4.5. Отладка программы в IDEA[#221089]

--- chapter_001/src/main/java/ru/job4j/condition/SqMaxError.java
+++ chapter_001/src/main/java/ru/job4j/condition/SqMaxError.java
@@ -6,15 +6,17 @@ public class SqMaxError {
         if (first > second) {
             if (first > third) {
                 if (first > forth) {
-                    result = first;       // - Было неправильное присвоение (result = third)
+                    result = first;      // - Было неправильное присваивание (result = third).
                 }
             }
         } else if (second > third) {
             if (second > forth) {
-                result = second;          // - Было неправильное присвоение (result = first)
+                result = second;         // - Было неправильное присваивание (result = first).
             }
-        } else if (third > forth) {
-            result = third;               // - Было неправильное присвоение (result = second)
+        }
+        if (third > forth && third > second && third > first) { // - Изменено условие и убран оператор else.
+
+            result = third;              // - Было неправильное присваивание (result = second).
         }
         return result;
     }
--- chapter_001/src/test/java/ru/job4j/condition/SqMaxErrorTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/SqMaxErrorTest.java
@@ -5,18 +5,50 @@ import org.junit.Test;
 import static org.hamcrest.core.Is.is;
 
 public class SqMaxErrorTest {
+
     @Test
     public  void findError () {
+        int out = SqMaxError.max(4, 2, 8, 5);
+        Assert.assertThat(out, is(8));
+    }
+    @Test
+    public  void findError2 () {
         int out = SqMaxError.max(1, 2, 3, 5);
         Assert.assertThat(out, is(5));
-
-        int out2 = SqMaxError.max(1, 2, 5, 3);
-        Assert.assertThat(out2, is(5));
-
-        int out3 = SqMaxError.max(1, 5, 3, 2);
-        Assert.assertThat(out3, is(5));
-
-        int out4 = SqMaxError.max(5, 2, 3, 1);
-        Assert.assertThat(out4, is(5));
+    }
+    @Test
+    public  void findError3 () {
+            int out = SqMaxError.max(1, 2, 5, 3);
+            Assert.assertThat(out, is(5));
+    }
+    @Test
+    public  void findError4 () {
+        int out = SqMaxError.max(1, 5, 3, 2);
+        Assert.assertThat(out, is(5));
+    }
+    @Test
+    public  void findError5 () {
+        int out = SqMaxError.max(5, 2, 3, 1);
+        Assert.assertThat(out, is(5));
+    }
+    @Test
+    public  void findError6 () {
+        int out = SqMaxError.max(9, 2, 3, 7);
+        Assert.assertThat(out, is(9));
+    }
+    @Test
+    public  void findError7 () {
+        int out = SqMaxError.max(6, 8, 3, 1);
+        Assert.assertThat(out, is(8));
+    }
+    @Test
+    public  void findError8 () {
+        int out = SqMaxError.max(8, 2, 3, 9);
+        Assert.assertThat(out, is(9));
+    }
+    @Test
+    public  void findError9 () {
+        int out = SqMaxError.max(5, 4, 3, 9);
+        Assert.assertThat(out, is(9));
     }
 }
