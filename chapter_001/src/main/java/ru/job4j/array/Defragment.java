09950124ff2ae79b404474b3e652a2f20d94ad4e
yor_email <daniil.stebnitsky@gmail.com>
2020-01-21 17:29:24 +0300

    6.8. Дефрагментация массива.[#221107]

---
+++ chapter_001/src/main/java/ru/job4j/array/Defragment.java
@@ -0,0 +1,22 @@
+package ru.job4j.array;
+
+public class Defragment {
+    public static String[] compress(String[] array) {
+        for(int i = 0; i < array.length; ++i) {
+            if(array[i] == null) {
+                int search = i;
+                while(array[search] == null && search < array.length - 1) {
+                    ++search;
+                    if(array[search] != null) {
+                        array[i] = array[search];
+                        array[search] = null;
+                        break;
+                    }
+                }
+            }
+            System.out.print(array[i]);
+        }
+        return array;
+    }
+}
+
---
+++ chapter_001/src/test/java/ru/job4j/array/DefragmentTest.java
@@ -0,0 +1,15 @@
+package ru.job4j.array;
+
+import org.junit.Assert;
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+
+public class DefragmentTest {
+    @Test
+    public void defragmenTest() {
+        String[] array = {null, null, null,"Hello", null," ", null, "world"};
+        String[] out = Defragment.compress(array);
+        String[] expected = {"Hello", " ", "world", null, null, null, null, null};
+        Assert.assertThat(out, is(expected));
+    }
+}
