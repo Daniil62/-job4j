dc58546143640203cc306fe6c1c5bd947fbf62f0
yor_email <daniil.stebnitsky@gmail.com>
2020-01-21 17:16:34 +0300

    6.7.2. Моно столбец в матрице.[#221112]

--- chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
+++ chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
@@ -11,5 +11,16 @@ public class MatrixCheck {
         }
         return result;
     }
+
+    public static boolean monoVertical(char[][] board, int col, char symbol) {
+        boolean result = true;
+        for(int i = 0; i < board.length - 1; ++i) {
+            if(board[i][col] != symbol) {
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
 }
 
--- chapter_001/src/test/java/ru/job4j/array/MatrixCheckTest.java
+++ chapter_001/src/test/java/ru/job4j/array/MatrixCheckTest.java
@@ -39,4 +39,45 @@ public class MatrixCheckTest {
         boolean out = MatrixCheck.monoHorizontal(board, 4, '*');
         Assert.assertThat(out, is(true));
     }
+
+//-----------------------------------------------------------------------------------------------------------
+
+    @Test
+    public void checkVerticalLine3x6T() {
+        char[][] board = {
+                {' ', '^', ' '},
+                {' ', '^', ' '},
+                {' ', '^', ' '},
+                {' ', '^', ' '},
+                {' ', '^', ' '},
+                {' ', '^', ' '}
+        };
+        boolean out = MatrixCheck.monoVertical(board, 1, '^');
+        Assert.assertThat(out, is(true));
+    }
+    @Test
+    public void checkVerticalLine5x5T() {
+        char[][] board = {
+                {' ', ' ', ' ', '@', ' '},
+                {' ', ' ', ' ', '@', ' '},
+                {' ', ' ', ' ', '@', ' '},
+                {' ', ' ', ' ', '@', ' '},
+                {' ', ' ', ' ', '@', ' '}
+        };
+        boolean out = MatrixCheck.monoVertical(board, 3, '@');
+        Assert.assertThat(out, is(true));
+    }
+    @Test
+    public void checkVerticalLine4x6F() {
+        char[][] board = {
+                {' ', ' ', ' ', '?'},
+                {' ', ' ', ' ', '?'},
+                {' ', ' ', ' ', 'W'},
+                {' ', ' ', ' ', '?'},
+                {' ', ' ', ' ', '?'},
+                {' ', ' ', ' ', '?'}
+        };
+        boolean out = MatrixCheck.monoVertical(board, 3, '?');
+        Assert.assertThat(out, is(false));
+    }
 }
