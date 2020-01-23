28accc00827d1ad27c9d94e8c64d89252297b4e8
yor_email <daniil.stebnitsky@gmail.com>
2020-01-23 17:36:25 +0300

    1.2. Вызов метода объекта[#221252]

---
+++ chapter_001/src/main/java/ru/job4j/oop/Student.java
@@ -0,0 +1,33 @@
+package ru.job4j.oop;
+
+public class Student {
+    private
+    String name;
+    String song = "\n\tYesterday,\n" +
+            "\tall my troubles seemed so far away,\n" +
+            "\tNow it looks as though they're here to stay.\n" +
+            "\tOh, I believe in yesterday.\n";
+    public
+    Student (String name) {
+        this.name = name;
+    }
+    String getName () {
+        return name;
+    }
+    void sing (String request) {
+        String answer = "";
+        if (request.equals(name + ", cпой, пожалуйста.")) {
+            answer = "-Конечно спою, чувак:" + song;
+        } else {
+            answer = "Те че надо? Отвали, я позову полицию!";
+        }
+        System.out.println(request);
+        System.out.println(answer);
+    }
+
+    public static void main(String[] args) {
+        Student student = new Student("Вован");
+        System.out.println("Чувак, как тебя зовут? " + "\n" + "-" + student.getName());
+        student.sing(student.getName() + ", cпой, пожалуйста.");
+    }
+}
