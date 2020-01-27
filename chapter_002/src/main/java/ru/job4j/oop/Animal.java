cc7c9cf72e61fdf34ae6ae7e24c12d90af9ec0f1
yor_email <daniil.stebnitsky@gmail.com>
2020-01-27 19:48:33 +0300

    2. Перегрузка конструктора.[#221262]

--- chapter_002/src/main/java/ru/job4j/oop/Animal.java
+++ chapter_002/src/main/java/ru/job4j/oop/Animal.java
@@ -1,7 +1,12 @@
 package ru.job4j.oop;
 
 public class Animal {
+    private String name;
     public Animal() {
-        System.out.println("Вызван конструктор класса Animal.");
+        System.out.println("Вызван конструктор по умолчанию класса Animal.");
+    }
+    public Animal(String name) {
+        this.name = name;
+        System.out.println("Вызван конструктор c параметром класса Animal.");
     }
 }
--- chapter_002/src/main/java/ru/job4j/oop/Tiger.java
+++ chapter_002/src/main/java/ru/job4j/oop/Tiger.java
@@ -2,10 +2,16 @@ package ru.job4j.oop;
 
 public class Tiger extends Predator {
     public Tiger() {
-        System.out.println("Вызов конструктора класса Tiger.");
+        System.out.println("Вызван конструктор по умолчанию класса Tiger.");
+    }
+    public Tiger(String name) {
+        super(name);
+        System.out.println("Вызван конструктор с параметром класса Tiger.");
     }
 
     public static void main(String[] args) {
         Tiger tiger = new Tiger();
+        Tiger secondTiger = new Tiger("tiger");
+        Animal animal = new Animal();
     }
 }
