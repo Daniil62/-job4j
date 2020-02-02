55466e4916433b72fd4eb6d3610288225250c153
yor_email <daniil.stebnitsky@gmail.com>
2020-02-02 22:42:30 +0300

    5.1. Тестирование. Подготовка данных.[#221228]

--- chapter_002/src/test/java/ru.job4j/StartUITest.java
+++ chapter_002/src/test/java/ru.job4j/StartUITest.java
@@ -3,6 +3,7 @@ package ru.job4j;
 import org.junit.Assert;
 import org.junit.Test;
 import ru.job4j.tracker.*;
+import static org.hamcrest.core.IsNull.nullValue;
 
 import static org.hamcrest.core.Is.is;
 
@@ -16,5 +17,26 @@ public class StartUITest {
         Item created = tracker.findAll()[0];
         Item expected = new Item("name");
         Assert.assertThat(created.getName(), is(expected.getName()));
+    }
+
+    @Test
+    public void whenReplaceItem() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("new item");
+        tracker.add(item);
+        String[] answers = {item.getId(), "changed item"};
+        StartUI.changeItem(new StubInput(answers), tracker);
+        Item replaced = tracker.findById(item.getId());
+        Assert.assertThat(replaced.getName(), is("changed item"));
+    }
+    @Test
+    public void whenItemDeleted() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("unnecessary item");
+        tracker.add(item);
+        String[] answers = {item.getId()};
+        StartUI.deleteItem(new StubInput(answers), tracker);
+        Item deleted = tracker.findById(item.getId());
+        Assert.assertThat(deleted, is(nullValue()));
     }
 }
