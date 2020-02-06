b433a3fad64288765f2b10e904126b4b5f442225
yor_email <daniil.stebnitsky@gmail.com>
2020-02-06 19:08:18 +0300

    9. Написать тесты на StartUI.[#221231]

--- chapter_002/src/test/java/ru.job4j/StartUITest.java
+++ chapter_002/src/test/java/ru.job4j/StartUITest.java
@@ -3,6 +3,10 @@ package ru.job4j;
 import org.junit.Assert;
 import org.junit.Test;
 import ru.job4j.tracker.*;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 import static org.hamcrest.core.IsNull.nullValue;
 
 import static org.hamcrest.core.Is.is;
@@ -13,7 +17,8 @@ public class StartUITest {
         String[] answers = {"name"};
         Input input = new StubInput(answers);
         Tracker tracker = new Tracker();
-        StartUI.createItem(input, tracker);
+        UserAction create = new CreateAction();
+        create.execute(input, tracker);
         Item created = tracker.findAll()[0];
         Item expected = new Item("name");
         Assert.assertThat(created.getName(), is(expected.getName()));
@@ -25,7 +30,8 @@ public class StartUITest {
         Item item = new Item("new item");
         tracker.add(item);
         String[] answers = {item.getId(), "changed item"};
-        StartUI.changeItem(new StubInput(answers), tracker);
+        UserAction replace = new ReplaceAction();
+        replace.execute(new StubInput(answers), tracker);
         Item replaced = tracker.findById(item.getId());
         Assert.assertThat(replaced.getName(), is("changed item"));
     }
@@ -35,8 +41,38 @@ public class StartUITest {
         Item item = new Item("unnecessary item");
         tracker.add(item);
         String[] answers = {item.getId()};
-        StartUI.deleteItem(new StubInput(answers), tracker);
+        UserAction delete = new DeleteAction();
+        delete.execute(new StubInput(answers), tracker);
         Item deleted = tracker.findById(item.getId());
         Assert.assertThat(deleted, is(nullValue()));
+    }
+    @Test
+    public void whenCheckOutput() {
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        PrintStream def = System.out;
+        System.setOut(new PrintStream(out));
+        Tracker tracker = new Tracker();
+        Item item = new Item("fix bug");
+        tracker.add(item);
+        ShowAllAction act = new ShowAllAction();
+        act.execute(new StubInput(new String[] {}), tracker);
+        String expect = ("=== Show all items ====" + System.lineSeparator() + item.getName()
+                + System.lineSeparator());
+        Assert.assertThat(new String(out.toByteArray()), is(expect));
+        System.setOut(def);
+    }
+    @Test
+    public void whenExitTrue() {
+        StubInput input = new StubInput(new String[] {"0"});
+        StubAction action = new StubAction();
+        new StartUI().init(input, new Tracker(), new UserAction[] {action});
+        Assert.assertThat(action.isCall(), is(true));
+    }
+    @Test
+    public void whenExitFalse() {
+        StubInput input = new StubInput(new String[] {"6"});
+        StubAction action = new StubAction();
+        new StartUI().init(input, new Tracker(), new UserAction[] {action});
+        Assert.assertThat(action.isCall(), is(false));
     }
 }
