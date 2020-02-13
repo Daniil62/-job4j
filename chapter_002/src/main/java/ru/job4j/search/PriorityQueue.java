6ea3c52af4f7e5551eca79ffc6f6540d4e3d07ca
yor_email <daniil.stebnitsky@gmail.com>
2020-02-13 14:08:33 +0300

    2. Очередь с приоритетом на LinkedList[#221186]

---
+++ chapter_002/src/main/java/ru/job4j/search/PriorityQueue.java
@@ -0,0 +1,49 @@
+package ru.job4j.search;
+
+import java.util.LinkedList;
+
+public class PriorityQueue {
+    private LinkedList<Task> tasks = new LinkedList<>();
+    public void put(Task task) {
+        int index = 0;
+        for (Task element : tasks) {
+            if (task.getPriority() < element.getPriority()) {
+                tasks.add(index, task);
+                break;
+            }
+            ++index;
+        }
+        if (index == tasks.size()) {
+            tasks.add(index, task);
+        }
+    }
+    public void showTasks() {
+        for (Task element : tasks) {
+            System.out.println(element.getDesc() + " " + element.getPriority() + System.lineSeparator());
+        }
+    }
+    public Task take() {
+        return tasks.remove(0);
+    }
+
+    public static void main(String[] args) {
+        PriorityQueue pq = new PriorityQueue();
+        Task task1 = new Task("task1", 4);
+        Task task2 = new Task("task2", 7);
+        Task task3 = new Task("task3", 8);
+        Task task4 = new Task("task4", 3);
+        Task task5 = new Task("task5", 6);
+        Task task6 = new Task("task6", 2);
+        Task task7 = new Task("task7", 1);
+        Task task8 = new Task("task8", 5);
+        pq.put(task1);
+        pq.put(task2);
+        pq.put(task3);
+        pq.put(task4);
+        pq.put(task5);
+        pq.put(task6);
+        pq.put(task7);
+        pq.put(task8);
+        pq.showTasks();
+    }
+}
