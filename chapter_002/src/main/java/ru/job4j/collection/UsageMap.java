2a2efe107765c74164cddf223f658b3811c2b041
yor_email <daniil.stebnitsky@gmail.com>
2020-02-18 12:43:46 +0300

    0. Map, HashMap[#221200]

---
+++ chapter_002/src/main/java/ru/job4j/collection/UsageMap.java
@@ -0,0 +1,16 @@
+package ru.job4j.collection;
+
+import java.util.HashMap;
+
+public class UsageMap {
+    public static void main(String[] args) {
+        HashMap<String, String> map = new HashMap<>();
+        map.put("email 1", "user1");
+        map.put("email 2", "user2");
+        map.put("email 3", "user3");
+        for (String key : map.keySet()) {
+            String value = map.get(key);
+            System.out.println(key + " = " + value);
+        }
+    }
+}
