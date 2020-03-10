4dd14fdc67865e3c422fc15aa2ba07026dfede65
yor_email <daniil.stebnitsky@gmail.com>
2020-03-10 19:39:08 +0300

    1. Collection API Улучшения +[#221069]

--- chapter_002/src/test/java/ru.job4j/ConvertListTest.java
+++ chapter_002/src/test/java/ru.job4j/ConvertListTest.java
@@ -13,18 +13,16 @@ import static org.junit.Assert.*;
 public class ConvertListTest {
     @Test
     public void whenTwoList() {
-        List<int[]> in = new ArrayList<>();
-        in.add(new int[] {1});
-        in.add(new int[] {2, 3});
+        List<int[]> in = new ArrayList<>(List.of(new int[] {1},
+                new int[] {2, 3}));
         List<Integer> expect = Arrays.asList(1, 2, 3);
         assertThat(ConvertList.convert(in), is(expect));
     }
     @Test
     public void whenThreeList() {
-        List<int[]> in = new ArrayList<>();
-        in.add(new int[] {1});
-        in.add(new int[] {2, 3});
-        in.add(new int[] {4, 5, 6, 7});
+        List<int[]> in = new ArrayList<>(List.of(new int[] {1},
+                new int[] {2, 3},
+                new int[] {4, 5, 6, 7}));
         List<Integer> expect = Arrays.asList(1, 2, 3, 4, 5, 6, 7);
         assertThat(ConvertList.convert(in), is(expect));
     }
